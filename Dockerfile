1734d018443f6aa0907473af6f714d6160dcece9
AngelKurten <angel@orandev.tech>
2020-08-03 17:15:40 -0500

    change

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
-FROM golang:1.8
-ENV VER 1.1.1
+FROM php:7.2-apache
 
+RUN apt update
+RUN apt-get install -y supervisor
+RUN mkdir -p /var/log/supervisor
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+
+WORKDIR /app
 COPY . ./
+RUN sed $'s/\r$//' ./run.sh > ./run.Unix.sh
 
-RUN chmod +x run.sh
 
+#CMD sh run.sh
+CMD ["/usr/bin/supervisord"]
 
-ENV BIND_ADDR 0.0.0.0:3123
-EXPOSE 3123
+EXPOSE 81
--- run.sh
+++ run.sh
@@ -4,13 +4,12 @@
     -spec-dir=architecture/                              \
     -default-assets-dir=assets/                          \
     -assets-dir=architecture/                            \
-    -bind-addr=0.0.0.0:3123                              \
+    -bind-addr=0.0.0.0:81                                \
     -spec-dir=api_specs/                                 \
     -spec-filename=logs.json                             \
     -spec-filename=services.json                         \
     -spec-filename=notifications.json                    \
-    -site-url=http://localhost:3123                      \
+    -site-url=http://localhost:81                        \
     -log-level=trace                                     \
     -author-show-assets=false                            \
     -theme=default
-
